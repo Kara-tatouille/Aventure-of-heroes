348b5684efaa33fbef56b78ed1db9ec23d972c43
censini <thibaut.surrel.dev@gmail.com>
2019-05-14 10:38:20 +0200

    modification de l'index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+</head>
+<body>
+    <?php require_once('./templates/header.html') ?>
+    <main>
+        <section>
+            <p>Descriptif du jeu</p>
+        </section>
+        <div>
+            <a href="./register.php">Jouer</a>
+        </div>
+    </main>
+</body>
+</html>
